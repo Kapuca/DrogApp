698f5f0f766f2ffd39a6b4d2bc5dfed42179b9ee
kapuca <jjan.bozic@gmail.com>
2020-09-08 16:37:24 +0200

    Add main component

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  template: `
+    <div id='main'>
+      <div class='tile' *ngFor='let tile of tiles' [routerLink]='tile' >
+        <img [src]="['/assets/img/home-' + tile + '-txt.png']"/>
+      </div>
+    </div>
+  `,
+  styles: [
+    '#main { display: grid;   grid-template-columns: auto auto; max-width: 100%;  background-color: #eed586; }',
+    '.tile { text-align: center;}',
+    'img { object-fit: scale-down; max-width: 100%;}',
+  ]
+})
+export class MainComponent implements OnInit {
+
+  tiles = ['droge', 'nasveti', 'zapleti', 'emergency', 'info', 'opozorila'];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
