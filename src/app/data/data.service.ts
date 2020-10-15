1862d4712e9b108cff98a326a7b213329e848d3c
kapuca <jjan.bozic@gmail.com>
2020-10-15 15:44:53 +0200

    Change backend endpoint

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,29 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  private url = 'https://littlechick-design.com/CLIENT/DA-backend/';
+  private url = 'https://drogapp.drogart.org/';
   private endParam = '.php?type=json';
 
   constructor(private http: HttpClient) { }
 
   getData(type: string, id?: number): Observable<any[]>{
-    const tmp = this.http.get<any[]>( this.url + type + this.endParam).pipe(map(arr => {
-      if (type === 'opozorila') { return arr; }
-      return arr.map(obj => {
-        const newObj = {};
-        Object.keys(obj).forEach(key => newObj[key.substring(1).toLowerCase()] = obj[key]);
-        return newObj;
-      });
-    }));
-    if (id) { return tmp.pipe(map(arr => arr.filter(obj => +obj['id'] === id))); }
-    return tmp;
+    try {
+      const tmp = this.http.get<any[]>( this.url + type + this.endParam).pipe(map(arr => {
+        if (type === 'opozorila') { return arr; }
+        return arr.map(obj => {
+          const newObj = {};
+          Object.keys(obj).forEach(key => newObj[key.substring(1).toLowerCase()] = obj[key]);
+          return newObj;
+        });
+      }),
+      catchError(error => {
+        throw new Error('Endpoint' + this.url + ' not reachable');
+      }));
+      if (id) { return tmp.pipe(map(arr => {
+        arr = arr.filter(obj => +obj['id'] === id);
+        if(arr.length > 0) { return arr; }
+        else { throw new Error('Data with ' + id + ' not found.'); }
+      })); }
+      return tmp;
+
+    } catch (error) {
+      return null;
+    }
   }
 
 }
