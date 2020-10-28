c9c9df3ee31cbfb85add499d44b52c36f63b6b8b
Matej De Faveri <md2085@student.uni-lj.si>
2020-10-28 22:44:34 +0100

    sem dodal nek folder, ki ga mogoče rabimo.. najbrž.. + spremenu, da SW pobere takoj SVG datoteke, da te vse naloži lepo

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -14,7 +14,7 @@ import { GeneralService } from '../general/general.service';
   styles: [
     '#main { position: absolute; top: 64px; display: grid; grid-template-columns: 50vw 50vw; grid-template-rows: repeat(3, 33.33%); grid-gap: 0px;}',
     '.tile { text-align: center; padding: 5vh 12px; }',
-    'img { object-fit: scale-down; width: 100%; max-height: 80%; }',
+    'img { object-fit: scale-down; width: 100%; max-height: 80%; margin-top: -5px; display: block; }',
 	 'p { font-family: "brandon_grotesque_regularRg"; color: #fff; text-transform: uppercase; font-weight: 600; letter-spacing: 0.15rem; }'
   ]
 })
