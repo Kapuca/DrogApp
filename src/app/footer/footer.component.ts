a07ad5100d17e0d22e954e535f68668150df4c54
kapuca <jjan.bozic@gmail.com>
2020-10-28 23:50:59 +0100

    Make emergency custom footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   template: `
   <div id="footer">
-    <p routerLink="/opozorila" onclick="this.blur();">
+    <p *ngIf="showKlic" ><a href="tel:112">KLIC 112</a></p>
+    <p *ngIf="!showKlic" routerLink="/opozorila" onclick="this.blur();">
       ZADNJE OPOZORILO
     </p>
   </div>
@@ -12,13 +14,22 @@ import { Component, OnInit } from '@angular/core';
   styles: [
     'div {position: fixed; bottom: 0px; background-color: #262626; height: 61px; width: 100%; line-height: 61px; box-sizing: border-box}',
     'p {margin: 0; text-align: center; color: white; }',
+    'a { color: inherit; text-align: inherit; text-decoration: inherit; }'
   ]
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  showKlic: boolean;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event['url']){
+        this.showKlic = event['url'] === '/emergency';
+      }
+  });
+
   }
 
 }
