13df79c5192d6d6978d67f4c1c2023e431499f1a
kapuca <jjan.bozic@gmail.com>
2020-10-29 18:44:17 +0100

    Account for loading icons in main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,15 +7,15 @@ import { GeneralService } from '../general/general.service';
     <div id='main'>
       <div class='tile' *ngFor='let title of titles()' [routerLink]="['/', title]" [style.background-color]='color(title)'>
         <img [src]="['assets/img/home-' + title + '.svg']"/>
-		  <p>{{title}}</p>
+		  <p>{{ title }}</p>
       </div>
     </div>
   `,
   styles: [
     '#main { position: absolute; top: 64px; display: grid; grid-template-columns: 50vw 50vw; grid-template-rows: repeat(3, 33.33%); grid-gap: 0px;}',
-    '.tile { text-align: center; padding: 5vh 12px; }',
-    'img { object-fit: scale-down; width: 100%; max-height: 80%; margin-top: -5px; display: block; }',
-	 'p { font-family: "brandon_grotesque_regularRg"; color: #fff; text-transform: uppercase; font-weight: 600; letter-spacing: 0.15rem; }'
+    '.tile { text-align: center; padding: 5vh 12px; content-align: center;}',
+    'img { object-fit: scale-down; max-width: 100%; height: 80%; display: block; margin: -5px auto auto auto;}',
+    'p { font-family: "brandon_grotesque_regularRg"; color: #fff; text-transform: uppercase; font-weight: 600; letter-spacing: 0.15rem; }'
   ]
 })
 export class MainComponent implements OnInit {
