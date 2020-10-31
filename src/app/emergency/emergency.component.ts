637f09b63aad60dec1cab9df978f9d2267d6a17f
Matej De Faveri <md2085@student.uni-lj.si>
2020-10-31 21:00:23 +0100

    Popravljen izgled za slike na emergencyju -> DA + Random stuff

--- src/app/emergency/emergency.component.ts
+++ src/app/emergency/emergency.component.ts
@@ -19,16 +19,18 @@ import { ActivatedRoute } from '@angular/router';
 					<button id="ne" class="izbira" [routerLink]="['./']" [queryParams]="{choice: false}" onclick="this.blur();">NE</button>
 				</div>
           </div>
-          <img src="assets/img/emergency_1.svg"/>
+          <img src="assets/img/emergency_1.svg" alt="PIC:EMERGENCY1"/>
         </div>
         <div *ngIf='choice === "true"'>
           <p class="basic-txt" [innerHTML]='emergency.da'></p>
-          <img src="assets/img/emergency_2.svg"/>
-          <img src="assets/img/emergency_3.svg"/>
+			 <ul class="emergencyDaSlike">
+				<li id="mergency2"><img src="assets/img/emergency_2.svg" alt="PIC:EMERGENCY2"/></li>
+				<li id="mergency3"><img id="mergency3Slika" src="assets/img/emergency_3.svg" alt="PIC:EMERGENCY3"/></li>
+			 </ul>
         </div>
         <div *ngIf='choice === "false"'>
           <p class="basic-txt" [innerHTML]='emergency.ne'></p>
-          <img src="assets/img/emergency_4.svg"/>
+          <img src="assets/img/emergency_4.svg" alt="PIC:EMERGENCY4"/>
         </div>
         </div>
     </div>
@@ -36,6 +38,8 @@ import { ActivatedRoute } from '@angular/router';
   styles: [
     '#choice-bar { margin: 20px 0px 20px 0px; display: flex; width: 100%; justify-content: space-around; background: #f4f4f4; height: 45px; font-size: 20px; padding-top: calc(45px/2 - 5px); }',
     'img { object-fit: scale-down; width: 90%; padding-left: 5%; height: fit-content; }',
+	 '.emergencyDaSlike > li { width: 100%; transform:rotateX(180deg); }',
+	 '#mergency3Slika { width: 85%; margin-top: -10%; }',
     '.izbira { font-size: 1.4rem; font-weight: 650; cursor: pointer; width: calc(50vw - 6px); height: 61.5px; top: -17px; position: relative; background-color: #00000000; border: none; }'
   ]
 })
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -12,8 +12,8 @@ import { RouterEvent, Router } from '@angular/router';
   </div>
   `,
   styles: [
-    'div {position: fixed; bottom: 0px; background-color: #262626; height: 61px; width: 100%; line-height: 61px; box-sizing: border-box}',
-    'p {margin: 0; text-align: center; color: white; font-weight: 600; letter-spacing: 0.4rem; }',
+    'div { position: fixed; bottom: 0px; background-color: #262626; height: 61px; width: 100%; line-height: 61px; box-sizing: border-box; }',
+    'p { margin: 0; text-align: center; color: white; font-weight: 600; letter-spacing: 0.25rem; font-size: 0.8rem; }',
   ]
 })
 export class FooterComponent implements OnInit {
