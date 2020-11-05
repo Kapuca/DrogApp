5f7c62690f56febd0431d6f3103a3a1d4380aef7
kapuca <jjan.bozic@gmail.com>
2020-11-05 15:47:14 +0100

    Fix tile color distribution

--- src/app/general/general.service.ts
+++ src/app/general/general.service.ts
@@ -7,19 +7,14 @@ export class GeneralService {
 
   tiles = ['droge', 'nasveti', 'zapleti', 'emergency', 'info', 'opozorila'];
   reserved = ['timer', 'chat', 'knofi', 'meter', 'kitty'];
-  colors = {
-    droge: '#eed586',
-    nasveti: '#dd6c56',
-    zapleti: '#c63f55',
-    emergency: '#653a4b',
-    info: '#49d3a3',
-    opozorila: '#31656d',
-    timer: '#eed586',
-    chat: '#dd6c56',
-    knofi: '#c63f55',
-    meter: '#653a4b',
-    kitty: '#49d3a3'
-  };
+  colors = [
+    '#eed586',
+    '#dd6c56',
+    '#c63f55',
+    '#653a4b',
+    '#49d3a3',
+    '#31656d'
+  ];
 
   constructor() { }
 
@@ -28,7 +23,7 @@ export class GeneralService {
   }
 
   getColor(tile: string){
-    return this.colors[tile];
+    return this.colors[(this.tiles.indexOf(tile) % 6) || 0];
   }
 
   addTile(tile: string): void {
