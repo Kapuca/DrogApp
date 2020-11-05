0fe5e86094a473fa66f4f32acbe0ed2d4a947ca9
Matej De Faveri <md2085@student.uni-lj.si>
2020-11-05 22:17:45 +0100

    Stil za list added

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -13,13 +13,12 @@ import { GeneralService } from '../general/general.service';
   `,
   styles: [
     '#main { position: absolute; top: 64px; display: grid; grid-template-columns: 50vw 50vw; grid-template-rows: repeat(6, 33.33%); grid-gap: 0px; }',
-    '.tile { text-align: center; padding: 5vh 12px; content-align: center;}',
-    'img { object-fit: scale-down; max-width: 100%; height: 80%; display: block; margin: -5px auto auto auto;}',
+    '.tile { text-align: center; padding: 5vh 12px; content-align: center; }',
+    'img { object-fit: scale-down; max-width: 100%; height: 80%; display: block; margin: -5px auto auto auto; }',
     'p { font-family: "brandon_grotesque_regularRg"; color: #fff; text-transform: uppercase; font-weight: 600; letter-spacing: 0.15rem; }'
   ]
 })
 export class MainComponent implements OnInit {
-
 
   constructor(private gs: GeneralService) { }
 
