e4799792d8afaa1e1d3b8b3b4e0615c3f8504a8c
kapuca <jjan.bozic@gmail.com>
2020-11-06 02:29:51 +0100

    Merge commit '4fa3415cdb403dab5f770fe6bd8ef23cba3b3690'

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -12,14 +12,13 @@ import { GeneralService } from '../general/general.service';
     </div>
   `,
   styles: [
-    '#main { position: absolute; top: 64px; display: grid; grid-template-columns: 50vw 50vw; grid-template-rows: repeat(6, 33.33%); grid-gap: 0px; }',
-    '.tile { text-align: center; padding: 5vh 12px; content-align: center;}',
-    'img { object-fit: scale-down; max-width: 100%; height: 80%; display: block; margin: -5px auto auto auto;}',
+    '#main { position: absolute; top: 64px; display: grid; grid-template-columns: 50vw 50vw; grid-template-rows: repeat(6, 33.33%); grid-gap: 0px; overflow-y: auto; overflow-x: hidden; }',
+    '.tile { text-align: center; padding: 5vh 12px; content-align: center; }',
+    'img { object-fit: scale-down; max-width: 100%; height: 80%; display: block; margin: -5px auto auto auto; }',
     'p { font-family: "brandon_grotesque_regularRg"; color: #fff; text-transform: uppercase; font-weight: 600; letter-spacing: 0.15rem; }'
   ]
 })
 export class MainComponent implements OnInit {
-
 
   constructor(private gs: GeneralService) { }
 
