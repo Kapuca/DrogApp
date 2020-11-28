b2cfcd35884ccea66764b4c69150075d1301ea07
sp0ng3Bob <Matej@SP0NG3B0B>
2020-11-28 18:48:41 +0100

    Dodan info o aplikaciji v info tab

--- src/app/opozorila/opozorila.component.ts
+++ src/app/opozorila/opozorila.component.ts
@@ -8,7 +8,7 @@ import { ActivatedRoute } from '@angular/router';
   <div id='main' class='secondPage'>
     <app-nav-header></app-nav-header>
     <div id='content'>
-      <div *ngFor='let opozorilo of opozorila; index as i' class='container' #container (click)='focusOn(i)'>
+      <div *ngFor='let opozorilo of opozorila; index as i' class='container' #container (click)='closeFirst(); focusOn(i);'>
         <h2 class="naslov">{{opozorilo.title | uppercase}}</h2>
         <p>{{ opozorilo.datetime }}</p>
         <div class='details' *ngIf='show[i]'>
@@ -21,7 +21,7 @@ import { ActivatedRoute } from '@angular/router';
   `,
   styles: [
     '.container:nth-child(2n+1) { background: #f4f4f4; }',
-    '.container { padding: 10px 15px; cursor: pointer; }',
+    '.container { padding: 10px 15px; cursor: pointer; scroll-padding-top: 109px; }',
     '.naslov { width: 100%; }',
     '.details { display: initial}',
     '.opozoriloICO { height: 3.5em; display: block; width: 100%; }',
@@ -32,11 +32,13 @@ export class OpozorilaComponent implements OnInit {
   opozorila: any[];
   @ViewChildren('container') containers: QueryList<ElementRef>;
   show: boolean[];
+  opozorilaListBox: any;
   constructor(private ds: DataService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.ds.getData('opozorila').subscribe(data => this.opozorila = data);
     this.show = [];
+	this.opozorilaListBox = document.getElementById("content").getBoundingClientRect();
     this.route.queryParams.subscribe(data => {
       this.show.fill(false);
       this.show[+data.show] = true;
@@ -45,11 +47,21 @@ export class OpozorilaComponent implements OnInit {
 
   focusOn(idx: number): void {
     const tmp = this.containers.toArray()[idx].nativeElement;
-    tmp.focus();
-    tmp.scrollIntoView({behavior: 'smooth', block: 'center'});
-    this.show.fill(false); // auto close other textst
+	
     if (this.show[idx]) { this.show[idx] = false; }
     else { this.show[idx] = true; }
+	
+	console.log(this.opozorilaListBox);
+ 	var box = tmp.getBoundingClientRect();
+	console.log(box)
+/*    var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+    console.log(Math.round(box.top +  scrollTop - clientTop));
+	tmp.scrollTo({behavior: 'smooth', top:  Math.round(box.top +  scrollTop)-120}); */
+	tmp.focus();
+  }
+  
+  closeFirst(): void {
+	this.show.fill(false);
   }
 
 }
