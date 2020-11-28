21360c7b97eec5035be246d3cee67f218b36661e
sp0ng3Bob <Matej@SP0NG3B0B>
2020-11-28 23:06:17 +0100

    Scroll into view na opozorilih spremenjen

--- src/app/opozorila/opozorila.component.ts
+++ src/app/opozorila/opozorila.component.ts
@@ -8,7 +8,7 @@ import { ActivatedRoute } from '@angular/router';
   <div id='main' class='secondPage'>
     <app-nav-header></app-nav-header>
     <div id='content'>
-      <div *ngFor='let opozorilo of opozorila; index as i' class='container' #container (click)='closeFirst(); focusOn(i);'>
+      <div *ngFor='let opozorilo of opozorila; index as i' class='container' #container (click)='focusOn(i);'>
         <h2 class="naslov">{{opozorilo.title | uppercase}}</h2>
         <p>{{ opozorilo.datetime }}</p>
         <div class='details' *ngIf='show[i]'>
@@ -33,35 +33,30 @@ export class OpozorilaComponent implements OnInit {
   @ViewChildren('container') containers: QueryList<ElementRef>;
   show: boolean[];
   opozorilaListBox: any;
+  lastOpenedTextHeight: number;
   constructor(private ds: DataService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.ds.getData('opozorila').subscribe(data => this.opozorila = data);
     this.show = [];
-	this.opozorilaListBox = document.getElementById("content").getBoundingClientRect();
+	this.opozorilaListBox = document.getElementById("content");
+	this.lastOpenedTextHeight = 0;
     this.route.queryParams.subscribe(data => {
       this.show.fill(false);
       this.show[+data.show] = true;
     });
   }
 
-  focusOn(idx: number): void {
-    const tmp = this.containers.toArray()[idx].nativeElement;
-	
-    if (this.show[idx]) { this.show[idx] = false; }
+  focusOn(idx: number): void {this.show.fill(false);
+	if (this.show[idx]) { this.show[idx] = false; }
     else { this.show[idx] = true; }
 	
-	console.log(this.opozorilaListBox);
+    const tmp = this.containers.toArray()[idx].nativeElement;
  	var box = tmp.getBoundingClientRect();
-	console.log(box)
-/*    var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
-    console.log(Math.round(box.top +  scrollTop - clientTop));
-	tmp.scrollTo({behavior: 'smooth', top:  Math.round(box.top +  scrollTop)-120}); */
-	tmp.focus();
-  }
-  
-  closeFirst(): void {
-	this.show.fill(false);
+	
+	if (document.getElementsByClassName('basic-txt')[0]) { this.lastOpenedTextHeight = document.getElementsByClassName('basic-txt')[0].getBoundingClientRect().height + 72; }
+	window.scrollTo({behavior: 'smooth', top: window.scrollY+(box.top-109-this.lastOpenedTextHeight)});
+	//tmp.focus();
   }
 
 }
