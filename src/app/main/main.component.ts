2a157fe1780832eed53d37a2ba16463c4e0f0464
kapuca <jjan.bozic@gmail.com>
2020-12-01 19:43:44 +0100

    Fix black background

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GeneralService } from '../general/general.service';
 import { slideInAnimation } from '../animations';
 
@@ -13,24 +13,20 @@ import { slideInAnimation } from '../animations';
     </div>
   `,
   styles: [
-    '#main { position: absolute; top: 64px; display: grid; grid-template-columns: 50vw 50vw; grid-template-rows: repeat(6, 33.33%); grid-gap: 0px; overflow-y: auto; overflow-x: hidden; }',
+    '#main {background-color: #262626; position: absolute; top: 64px; display: grid; grid-template-columns: 50vw 50vw; grid-template-rows: repeat(6, 33.33%); grid-gap: 0px; overflow-y: hidden; overflow-x: hidden; }',
     '.tile { text-align: center; padding: 5vh 12px; content-align: center; }',
     'img { object-fit: scale-down; max-width: 100%; height: 80%; display: block; margin: -5px auto auto auto; }',
     'p { font-family: "brandon_grotesque_regularRg"; color: #fff; text-transform: uppercase; font-weight: 600; letter-spacing: 0.15rem; }'
   ],
   animations: [
-	slideInAnimation
+    slideInAnimation
   ]
 })
-export class MainComponent implements OnInit, OnDestroy {
+export class MainComponent implements OnInit {
 
   constructor(private gs: GeneralService) { }
 
   ngOnInit(): void { }
-
-  ngOnDestroy(): void {
-    document.body.style.backgroundColor = 'white';
-  }
 
   titles(){ return this.gs.getTitles(); }
   color(title){ return this.gs.getColor(title); }
