7c17156968c8ddc102bdcc2971125db39d1fbcfb
sp0ng3Bob <Matej@SP0NG3B0B>
2020-12-02 03:58:40 +0100

    Emergency ALERT BUTTON ob spremembi stanja

--- src/app/emergency/emergency.component.ts
+++ src/app/emergency/emergency.component.ts
@@ -19,25 +19,28 @@ import { ActivatedRoute } from '@angular/router';
         </div>
         <div *ngIf='choice === "false"'>
           <p class="basic-txt" [innerHTML]='emergency.ne'></p>
-          <button id="choice-bar" class="izbira" [routerLink]="['./']" [queryParams]="{choice: null}" onclick="this.blur();">< Nazaj</button>
-			 <ul class="emergencyDaSlike">
-				<li id="mergency2"><img src="assets/img/emergency_2.svg" alt="PIC:EMERGENCY2"/></li>
-				<li id="mergency3"><img id="mergency3Slika" src="assets/img/emergency_3.svg" alt="PIC:EMERGENCY3"/></li>
-			 </ul>
+          <button class="choice-bar-back" [routerLink]="['./']" [queryParams]="{choice: null}" onclick="this.blur();"><span>sprememba stanja</span></button>
+		  <ul class="emergencyNeSlike">
+			<li id="mergency2"><img src="assets/img/emergency_2.svg" alt="PIC:EMERGENCY2"/></li>
+			<li id="mergency3"><img id="mergency3Slika" src="assets/img/emergency_3.svg" alt="PIC:EMERGENCY3"/></li>
+		  </ul>
         </div>
-        <div *ngIf='choice === "true"'>
+        <div *ngIf='choice === "true"' id="emergencyDa">
           <p class="basic-txt" [innerHTML]='emergency.da'></p>
-          <button id="choice-bar" class="izbira" [routerLink]="['./']" [queryParams]="{choice: null}" onclick="this.blur();">< Nazaj</button>
+          <button class="choice-bar-back" [routerLink]="['./']" [queryParams]="{choice: null}" onclick="this.blur();"><span>sprememba stanja</span></button>
           <img src="assets/img/emergency_4.svg" alt="PIC:EMERGENCY4"/>
         </div>
-        </div>
+      </div>
     </div>
   `,
   styles: [
     '#choice-bar { margin: 20px 0px 20px 0px; display: flex; width: 100%; justify-content: space-around; background: #f4f4f4; height: 45px; font-size: 20px; padding-top: calc(45px/2 - 5px); }',
+    '.choice-bar-back { z-index: 2; position: absolute; top: 25vh; right: 0px; width: 40px; height: max-content; cursor: pointer; background: repeating-linear-gradient(45deg, #d40202bd, #d40202bd 10px, #ff0000a1 10px, #ff0000a1 20px); }',
+	'.choice-bar-back > span { padding: 7px 0px 7px 0px; writing-mode: vertical-rl; text-orientation: upright; text-transform: uppercase; font-size: 1.3em; color: white; font-family: "gotham_mediumregular"; }',
     'img { object-fit: scale-down; width: 90%; padding-left: 5%; height: fit-content; }',
-    '.emergencyDaSlike > li { width: 100%; transform:rotateX(180deg); }',
-    '#mergency3Slika { width: 85%; margin-top: -10%; }',
+    '.emergencyNeSlike > li { width: 100%; transform:rotateX(180deg); }',
+    '#mergency3Slika { width: 77%; margin-top: -17%; }',
+	'#content { overflow: visible !important; }',
     '.izbira { font-size: 1.4rem; font-weight: 650; cursor: pointer; width: calc(50vw - 6px); height: 61.5px; top: -17px; position: relative; background-color: #00000000; border: none; }'
   ]
 })
