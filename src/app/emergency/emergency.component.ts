56c4a7f51f589917fa65c184e2a3c22536ec73e4
sp0ng3Bob <Matej@SP0NG3B0B>
2020-12-06 23:20:53 +0100

    More emergency style fixes

--- src/app/emergency/emergency.component.ts
+++ src/app/emergency/emergency.component.ts
@@ -15,31 +15,33 @@ import { ActivatedRoute } from '@angular/router';
             <div class="vertical"></div>
 		      	<button id="ne" class="izbira" [routerLink]="['./']" [queryParams]="{choice: false}" onclick="this.blur();">NE</button>
           </div>
-          <img src="assets/img/emergency_1.svg" alt="PIC:EMERGENCY1" oncontextmenu="return false;"/>
+          <img id="mergency1Slika" src="assets/img/emergency_1.svg" alt="PIC:EMERGENCY1" oncontextmenu="return false;"/>
         </div>
         <div *ngIf='choice === "false"'>
           <p class="basic-txt" [innerHTML]='emergency.ne'></p>
           <button class="choice-bar-back" [routerLink]="['./']" [queryParams]="{choice: null}" onclick="this.blur();"><span>sprememba stanja</span></button>
 		  <ul class="emergencyNeSlike">
-			<li id="mergency2"><img src="assets/img/emergency_2.svg" alt="PIC:EMERGENCY2" oncontextmenu="return false;"/></li>
-			<li id="mergency3"><img id="mergency3Slika" src="assets/img/emergency_3.svg" alt="PIC:EMERGENCY3" oncontextmenu="return false;"/></li>
+			<li><img id="mergency2Slika" src="assets/img/emergency_2.svg" alt="PIC:EMERGENCY2" oncontextmenu="return false;"/></li>
+			<li><img id="mergency3Slika" src="assets/img/emergency_3.svg" alt="PIC:EMERGENCY3" oncontextmenu="return false;"/></li>
 		  </ul>
         </div>
         <div *ngIf='choice === "true"' id="emergencyDa">
           <p class="basic-txt" [innerHTML]='emergency.da'></p>
           <button class="choice-bar-back" [routerLink]="['./']" [queryParams]="{choice: null}" onclick="this.blur();"><span>sprememba stanja</span></button>
-          <img src="assets/img/emergency_4.svg" alt="PIC:EMERGENCY4" oncontextmenu="return false;"/>
+          <img id="mergency4Slika" src="assets/img/emergency_4.svg" alt="PIC:EMERGENCY4" oncontextmenu="return false;"/>
         </div>
       </div>
     </div>
   `,
   styles: [
     '#choice-bar { margin: 20px 0px 20px 0px; display: flex; width: 100%; justify-content: space-around; background: #f4f4f4; height: 45px; font-size: 20px; padding-top: calc(45px/2 - 5px); }',
-    '.choice-bar-back { z-index: 2; position: fixed; top: 35vh; right: 0px; width: 35px; height: max-content; cursor: pointer; background: repeating-linear-gradient(45deg, #d40202bd, #d40202bd 10px, #ff0000a1 10px, #ff0000a1 20px); }',
+    '.choice-bar-back { z-index: 2; position: fixed; top: 38vh; right: 0px; width: 37px; height: max-content; cursor: pointer; background: repeating-linear-gradient(45deg, #d40202bd, #d40202bd 10px, #ff0000a1 10px, #ff0000a1 20px); }',
     '.choice-bar-back > span { padding: 7px 0px 7px 0px; writing-mode: vertical-rl; text-orientation: upright; text-transform: uppercase; font-size: 1.05em; color: white; font-family: "gotham_mediumregular"; }',
-    'img { object-fit: scale-down; width: 90%; padding-left: 5%; height: fit-content; }',
+    'img { object-fit: scale-down; width: 95%; height: auto; padding: 15px 0px 0px 0px; }',
     '.emergencyNeSlike > li { width: 100%; transform:rotateX(180deg); }',
-    '#mergency3Slika { width: 77%; margin-top: -17%; }',
+    '#mergency2Slika { margin-top: 10%; }',
+    '#mergency3Slika { height: 82%; margin-top: 1%; }',
+    '#mergency1Slika, #mergency4Slika { margin: 0px 2.5%; }',
     '.izbira { font-size: 1.4rem; font-weight: 650; cursor: pointer; width: calc(50vw - 6px); height: 61.5px; top: -17px; position: relative; background-color: #00000000; border: none; }',
 	//'p.basic-txt { padding: 20px 36px 20px 20px; }'
   ]
