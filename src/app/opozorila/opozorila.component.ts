039781013d310e27875ac67f65bc8560f954410c
kapuca <jjan.bozic@gmail.com>
2020-12-06 19:25:44 +0100

    Reimplement view scrolling on opozorila

--- src/app/opozorila/opozorila.component.ts
+++ src/app/opozorila/opozorila.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, AfterViewChecked, QueryList, ViewChildren, OnDestroy } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChildren, OnDestroy } from '@angular/core';
 import { DataService } from '../data/data.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -9,10 +9,10 @@ import { ActivatedRoute } from '@angular/router';
     <app-nav-header></app-nav-header>
     <div id='content'>
       <div *ngFor='let opozorilo of opozorila' class='container' #container [id]='"container-" + opozorilo.id'>
-		<div class="opozorilaItemHeader" (click)='focusOn(opozorilo.id);'>
-		  <h2 class="naslov">{{opozorilo.title | uppercase}}</h2>
-          <p>{{ opozorilo.datetime }}</p>
-		</div>
+        <div class="opozorilaItemHeader" (click)='focusOn(opozorilo.id);'>
+          <h2 class="naslov">{{opozorilo.title | uppercase}}</h2>
+              <p>{{ opozorilo.datetime }}</p>
+        </div>
         <div class='details' *ngIf='show == opozorilo.id'>
           <p class="basic-txt" [innerHTML]='opozorilo.msg'></p>
           <a [href]='opozorilo.link'><img class="opozoriloICO" src='assets/img/more.svg' oncontextmenu="return false;"/></a>
@@ -27,42 +27,27 @@ import { ActivatedRoute } from '@angular/router';
     '.naslov { width: 100%; }',
     '.details { display: initial}',
     '.opozoriloICO { height: 3.5em; display: block; width: 100%; }',
-	'#content { animation: 0.6s ease-out 0s 1 slideFromUp; }'
+    '#content { animation: 0.6s ease-out 0s 1 slideFromUp; }'
   ]
 })
-export class OpozorilaComponent implements OnInit, AfterViewChecked, OnDestroy{
+export class OpozorilaComponent implements OnInit, OnDestroy{
 
   opozorila: any[];
   @ViewChildren('container') containers: QueryList<ElementRef>;
   show: number;
-  showId: number;
-  opozorilaListBox: any;
-  lastOpenedTextHeight: number;
-  specialCase: boolean;
+  observer: MutationObserver;
 
   constructor(private ds: DataService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.ds.getData('opozorila').subscribe(data => this.opozorila = data);
-    this.opozorilaListBox = document.getElementById('content');
-    this.lastOpenedTextHeight = 0;
-	this.specialCase = false;
     this.route.queryParams.subscribe(data => {
       this.show = data.show;
-	  this.specialCase = true;
     });
-    this.ds.getSubscribed();
-  }
-  
-  ngAfterViewChecked(): void {
-	if (this.specialCase) {
-	  this.specialCase = false;
-	  window.scrollTo({behavior: 'smooth', top: this.opozorilaListBox.getBoundingClientRect().height});
-	}
-	if (typeof this.show == 'number' && this.specialCase) {
-		this.specialCase = false;
-		this.showId = this.containers.toArray().indexOf(this.containers.toArray().filter(el => el.nativeElement.id === ('container-' + this.show))[0]);
-	}
+    // construct mechanism to be triggered after old shown el closes
+    this.observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => this.scrollView());
+    });
   }
 
   ngOnDestroy(): void {
@@ -70,30 +55,27 @@ export class OpozorilaComponent implements OnInit, AfterViewChecked, OnDestroy{
   }
 
   focusOn(idx: number): void {
-    if (this.show !== idx) {
-	  this.show = idx;
-      const tmp = this.containers.toArray().filter(el => el.nativeElement.id === ('container-' + idx))[0];
-	  const tmpId = this.containers.toArray().indexOf(tmp);
-      const box = tmp.nativeElement.getBoundingClientRect();
+    /* Logging
+    const shownEl = this.containers.filter(el => el.nativeElement.id === ('container-' + this.show))[0];
+    const elTOshow = this.containers.filter(el => el.nativeElement.id === ('container-' + idx))[0];
+    const se = shownEl.nativeElement.getBoundingClientRect();
+    const ets = elTOshow.nativeElement.getBoundingClientRect();
+    console.log('focusOn shownEl', se.y, se.height);
+    console.log('focusOn elTOshow', ets.y, ets.height);
+    */
+    const shownEl = this.containers.filter(el => el.nativeElement.id === ('container-' + this.show))[0];
+    this.show = idx;
+    if (shownEl) {
+      this.observer.observe( shownEl.nativeElement, {childList: true} );
+    }else{
+      this.scrollView();
+    }
 
-      if (typeof this.showId == 'number' && this.showId > tmpId) {
-        window.scrollTo({behavior: 'smooth', top: window.scrollY - (109 - box.top)});
-      } else {
-        if (document.getElementsByClassName('basic-txt')[0]) {
-          this.lastOpenedTextHeight = document.getElementsByClassName('basic-txt')[0].getBoundingClientRect().height + 72;
-        } 
- 		else if (this.containers.toArray()[this.containers.toArray().length-1].nativeElement.attributes['id'].nodeValue.split('-')[1] == idx) {
-		  this.specialCase = true;
-		}
-		window.scrollTo({behavior: 'smooth', top: window.scrollY + (box.top - 109 - this.lastOpenedTextHeight)});
-      }
-	  
-	  this.showId = tmpId;
-      this.ds.getSubscribed();
-    } else {
-	  this.show = -1;
-	  this.lastOpenedTextHeight = 0;
-	}
+  }
+
+  scrollView() {
+      const elTOshow = this.containers.filter(el => el.nativeElement.id === ('container-' + this.show))[0];
+      window.scrollBy({top: elTOshow.nativeElement.getBoundingClientRect().top - 109, left: 0, behavior: 'smooth'});
   }
 
 }
