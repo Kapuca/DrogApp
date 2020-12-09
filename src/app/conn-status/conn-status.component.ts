222b1d3a9e4d0c743e5bb6dc23e649e7ae9bc7f8
kapuca <jjan.bozic@gmail.com>
2020-12-09 07:56:24 +0100

    Make conn-status responsive

--- src/app/conn-status/conn-status.component.ts
+++ src/app/conn-status/conn-status.component.ts
@@ -1,40 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { DataService } from '../data/data.service';
 
 @Component({
   selector: 'app-conn-status',
   template: `
-      <svg height="64" width="32">
-        <circle cx="16" cy="32" r="10" [attr.stroke]="statusColor" stroke-width="4" fill="none" />
+  <div #bckg class='bckg' [style.position]="(collapseBottom ? 'absolute' : 'static')">
+    <div #centre class='centre'>
+      <svg height="32" width="32">
+        <circle cx="16" cy="16" r="10" [attr.stroke]="'#000000'" stroke-width="4" fill="none" />
       </svg>
-      <p class="helper-txt" [innerHTML]="statusText"></p>
+      <p [innerHTML]="statusText"></p>
+    </div>
+  </div>
   `,
   styleUrls: ['./conn-status.component.css'],
 
 })
-export class ConnStatusComponent implements OnInit {
+export class ConnStatusComponent implements OnInit, AfterViewInit{
 
-  statusColor: string;
+  @Input() collapseBottom = true;
+  @Input() persist: boolean;
+  @ViewChild('bckg') container: ElementRef;
+  @ViewChild('centre') wrapper: ElementRef;
+  @Input() timeout = 2000;
+  opened: boolean;
   statusText: string;
 
   constructor(private ds: DataService) { }
 
   ngOnInit(): void {
-    this.updateOnlineStatus(this.ds.isOnline());
     this.ds.onStatusChange( status => this.updateOnlineStatus(status) );
   }
 
-  updateOnlineStatus(isOnline): void {
-    if (isOnline) {
-      this.statusColor = '#4caf50';
-      this.statusText = 'Vaša naprava <i>ima</i> povezavo do interneta.';
-    } else {
-      this.statusColor = '#e91e63';
-      this.statusText = 'Vaša naprava <i>nima</i> povezave do interneta.';
+  ngAfterViewInit() {
+    if (this.persist || !this.ds.isOnline()) {
+      this.updateOnlineStatus(this.ds.isOnline());
     }
-
-    console.log(this.statusColor, this.statusText);
   }
+
+  updateOnlineStatus(isOnline): void {
+    console.log('updateOnlineStatus', isOnline, this.persist);
+    if (isOnline) {
+      this.showMe();
+      this.openMe(isOnline);
+
+      setTimeout(() => {
+        this.closeMe(isOnline);
+        if (!this.persist) { setTimeout(() => this.hideMe(), this.timeout / 2 ); }
+      }, this.timeout);
+
+    } else {
+      this.showMe();
+      this.openMe(isOnline);
+      setTimeout(() => this.closeMe(isOnline), this.timeout);
+    }
+  }
+
+  closeMe(isOnline: boolean): void {
+    this.wrapper.nativeElement.style.display = 'none';
+    this.container.nativeElement.style.backgroundColor = isOnline ? '#4caf50' : '#e91e63';
+    this.container.nativeElement.style.height = '5px';
+  }
+
+  openMe(isOnline: boolean) {
+    this.wrapper.nativeElement.style.display = 'block';
+    this.container.nativeElement.style.backgroundColor = (isOnline ? '#4caf50' : '#e91e63') + 'a0';
+    this.statusText = 'Internetna povezava ' + (isOnline ? 'vzpostavljena' : 'prekinjena') + '.';
+    this.container.nativeElement.style.height = '100%';
+  }
+
+  hideMe() {
+    this.container.nativeElement.style.display = 'none';
+  }
+
+  showMe() {
+    this.container.nativeElement.style.display = 'block';
+  }
+
 
 
 
