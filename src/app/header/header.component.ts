48f8ad0951ecdf5eaf4eaa6f6c7b29595ee92c75
sp0ng3Bob <Matej@SP0NG3B0B>
2020-12-11 23:54:48 +0100

    Header content fix to accomodate outline style error

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,13 +5,13 @@ import { Component, OnInit } from '@angular/core';
   template: `
   <div id="header">
     <p routerLink="/" onclick="this.blur();">
-      DROG<span style="font-family:gotham_mediumregular; font-weight:bold">APP</span>
-    </p>
+	  &nbsp;DROG<span style="font-family:gotham_mediumregular; font-weight:bold">APP</span>
+	</p>
   </div>
   `,
   styles: [
     'div { position: fixed; top: 0px; width: 100vw; background-color: #262626; letter-spacing: 5vw; height: 64px; line-height: 64px; box-sizing: border-box; z-index: 420; user-select: none; }',
-    'p { margin: 0; text-align: center; color: white; font-size: 2em; transform: translateX(2.5vw); }'
+    'p { margin: 0; text-align: center; color: white; font-size: 2em; outline-offset: -1px; }',
   ]
 })
 export class HeaderComponent implements OnInit {
